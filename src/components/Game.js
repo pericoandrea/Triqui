e6c3e0bfc8b7ebb0876bf711c1963e2a236e4f1d
María Andrea <pericoandrea@gmail.com>
2020-03-31 10:37:14 -0500

    Mensaje de Game

--- src/components/Game.js
+++ src/components/Game.js
@@ -15,7 +15,7 @@ import Board from './Board';
         }
         
         calculateWinner = (squares) => {
-          
+          // Este es otro cambio
             const lines = [
                 [0, 1, 2],
                 [3, 4, 5],
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import Game from './components/Game';
 
   ReactDOM.render(
+    //Otro comentario
     <Game />,
     document.getElementById('root')
   );
