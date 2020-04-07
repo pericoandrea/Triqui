9e50d5f335faca31f5484c7254434489925d6167
Moreno Moreno <drmorenom@indracompany.com>
2020-04-07 15:09:50 -0500

    empezando ppt

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Game from "./components/Triqui/Game";
 import Home from "./components/Home";
+import saludo from "./components/ppt/game1"
 
 function App() {
     return (
         <Switch>
             <Route path="/" component={Home} exact/>
             <Route path="/triqui" component={Game} />
+            <Route path="/ppt" component={saludo}/>
+        
+         
         </Switch>
     )
 }
---
+++ src/components/ppt/game1.js
@@ -0,0 +1,59 @@
+// import React, { Component } from "react";
+// import ReactDOM from "react";
+import React from "react"
+
+let name="David";
+function saludo(name){
+    return ( 
+
+    <h1> Hola Mundo, saludo</h1>
+    console.log(saludo);
+    
+
+
+}
+
+export default saludo;
+ // var usuarioElige = prompt("piedra, papel o tijera?");
+ 
+ 
+//          }
+//      }
+
+// //      render(){ 
+//         const opciones = ["piedra", "papel", "tijera"];
+//          let numAleatorio = Math.floor(Math.random() * 3); // 0 , 1, 2
+//          let computadoraElige = opciones[numAleatorio];
+//         let comparar = function( usuarioElige, computadoraElige){
+//        if(usuarioElige === computadoraElige){
+//            return "¡Es un empate!";
+//       }
+//        if(usuarioElige === "piedra"){
+//            if(computadoraElige === "tijera"){
+//                return "¡Gana usuario, Piedra gana!";
+//            } else {
+//                return "¡Computador Gana, Papel gana!";
+//            }
+//       }
+//       if(usuarioElige === "papel"){
+//           if(computadoraElige === "piedra"){
+//               return "¡Gana Usuario, Papel gana!";
+//           } else {
+//               return "¡Gana Computador, Tijera gana!";
+//           }
+//       }
+//      if(usuarioElige === "tijera"){
+//           if(computadoraElige === "papel"){
+//              return "¡Gana usuario, Tijera gana!";
+//           } else {
+//               return "¡Computador Gana, Piedra gana!";
+//           }
+//       }
+//   }
+//   comparar(usuarioElige, computadoraElige);
+//              // console.log(usuarioElige,computadoraElige)
+//         );
+//      }
+//  }
+
+//  export default Game1;
